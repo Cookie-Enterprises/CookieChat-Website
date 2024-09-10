8ebe211d2accbfff6f79025cd1e9e3ddbbe51daf
DeadPumpkin <masonyeets234@gmail.com>
2024-09-09 22:01:10 -0400

    Add Breakpoints and Create Feed Page

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
+import "./pages/CookieFeed.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   routes: [
     {
       path: "/",
+      component: Home,
+    },
+    {
+      path: "/feed/",
       component: Home,
     },
   ],
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="flex flex-col h-full">
-    <h1 class="font-bold text-8xl text-center flex-grow self-start mt-[2rem]">
+  <div class="flex flex-col h-full mx-auto">
+    <h1
+      class="font-semibold font-['Inter'] text-8xl md:text-[7rem] text-center flex-grow md:mt-12 self-start mt-[2rem] lg:mt-16 lg:text-[10rem]"
+    >
       Cookie<wbr />Chat
     </h1>
     <button
-      class="self-end text-center m-auto box-border border-4 border-black py-1 w-80 mb-2 rounded-lg font-['Arvo'] bg-[#f3d9a5]"
+      class="self-end text-center m-auto box-border border-4 md:border-[6px] lg:text-lg md:text-lg lg:mb-4 md:mb-3 md:py-1.5 lg:py-2 border-black py-1 w-80 lg:border-8 lg:w-[40rem] md:w-[30rem] mb-2 lg:mt-4 md:mt-3 rounded-lg font-['Arvo'] lg:rounded-xl bg-[#f3d9a5]"
     >
       Sign in with Google
     </button>
     <button
-      class="self-end text-center m-auto mb-[2rem] py-1 border-4 border-black w-80 mt-2 rounded-lg bg-[#f3d9a5]"
+      class="self-end text-center lg:text-lg m-auto mb-[2rem] lg:mb-[4rem] py-1 lg:py-2 lg:border-8 border-4 border-black w-80 lg:w-[40rem] mt-2 lg:mt-4 rounded-lg lg:rounded-xl bg-[#f3d9a5] md:mt-3 md:w-[30rem] md:py-1.5 md:mb-4 md:border-[6px] md:text-lg"
     >
       Sign in with Apple
     </button>
